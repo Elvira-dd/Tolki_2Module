0726135f12ebca889c659c0fdbd24ebe1eaff399
Elvira-dd <111658129+Elvira-dd@users.noreply.github.com>
2024-11-11 15:46:21 +0300

    Initial Commit

---
+++ Tolki/TolkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TolkiApp.swift
+//  Tolki
+//
+//  Created by Эльвира on 11.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct TolkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TolkiTests/TolkiTests.swift
@@ -0,0 +1,17 @@
+//
+//  TolkiTests.swift
+//  TolkiTests
+//
+//  Created by Эльвира on 11.11.2024.
+//
+
+import Testing
+@testable import Tolki
+
+struct TolkiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
